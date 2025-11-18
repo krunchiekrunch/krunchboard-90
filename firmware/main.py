5964096681b72bff5d16071dc7337204ec45fda5
krunch <75719839+krunchiekrunch@users.noreply.github.com>
2025-11-18 13:46:05 +0000

    updated formatting

--- firmware/main.py
+++ firmware/main.py
@@ -2,19 +2,24 @@ import board
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import MatrixScanner
 from kmk.keys import KC
-from kmk.modules.encoder import EncoderHandler
 from kmk.scanners import DiodeOrientation
+from kmk.extensions.display import Display, TextEntry
+from kmk.extensions.display.ssd1306 import SSD1306
 from kmk.extensions.RGB import RGB, AnimationModes
-from kmk.extensions.LED import LED
 from kmk.modules.macros import Macros, Press, Release, Tap
-from kmk.modules.layers import Layers
+from kmk.modules.mouse_keys import MouseKeys
 from kmk.extensions.media_keys import MediaKeys
+from kmk.modules.encoder import EncoderHandler
 
 keyboard = KMKKeyboard()
+
 macros = Macros()
 keyboard.modules.append(macros)
+mouse = MouseKeys()
+keyboard.modules.append(MouseKeys())
 keyboard.extensions.append(MediaKeys())
-keyboard.modules.append(Layers())
+encoder_handler = EncoderHandler()
+keyboard.modules.append(encoder_handler)
 
 # matrix
 keyboard.row_pins = (
@@ -48,35 +53,39 @@ keyboard.col_pins = (
 
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
-BASE = 0
-FN = 1
+_______ = KC.TRNS
+xxxxxxx = KC.NO
+xxxx = KC.NO
+Fn = KC.MO(1)
+
 
 keyboard.keymap = [
     # Base (Layer 0)
     [
-        KC.ESC,   KC.F1,     KC.F2, KC.F3,   KC.F4, KC.F5, KC.F6,    KC.F7, KC.F8, KC.F9,    KC.F10,  KC.F11,    KC.F12,   KC.NLCK,   KC.SLCK, KC.INS,   KC.MUTE,
-        KC.GRAVE, KC.N1,     KC.N2, KC.N3,   KC.N4, KC.N5, KC.N6,    KC.N7, KC.N8, KC.N9,    KC.N0,   KC.MINUS,  KC.EQUAL, KC.NO,     KC.BSPC, KC.HOME,  KC.NO,
-        KC.TAB,   KC.NO,     KC.Q,  KC.W,    KC.E,  KC.R,  KC.T,     KC.Y,  KC.U,  KC.I,     KC.O,    KC.P,      KC.LBRC,  KC.RBRC,   KC.BSLS, KC.PGUP,  KC.NO,
-        KC.CAPS,  KC.NO,     KC.A,  KC.S,    KC.D,  KC.F,  KC.G,     KC.H,  KC.J,  KC.K,     KC.L,    KC.SCOLON, KC.QUOTE, KC.ENTER,  KC.NO,   KC.PGDN,  KC.NO,
-        KC.NO,    KC.LSHIFT, KC.Z,  KC.X,    KC.C,  KC.V,  KC.B,     KC.N,  KC.M,  KC.COMMA, KC.DOT,  KC.SLASH,  KC.NO,    KC.RSHIFT, KC.UP,   KC.END,   KC.NO,
-        KC.LCTRL, KC.LGUI,   KC.NO, KC.LALT, KC.NO, KC.NO, KC.SPACE, KC.NO, KC.NO, KC.NO,    KC.RALT, KC.MO(FN), KC.RCTRL, KC.LEFT,   KC.DOWN, KC.RIGHT, KC.NO,
+        KC.ESC,   KC.F1,   KC.F2, KC.F3,   KC.F4, KC.F5, KC.F6,    KC.F7, KC.F8, KC.F9,    KC.F10,  KC.F11,    KC.F12,   KC.NLCK,  KC.SLCK, KC.INS,   KC.MUTE,
+        KC.GRAVE, KC.N1,   KC.N2, KC.N3,   KC.N4, KC.N5, KC.N6,    KC.N7, KC.N8, KC.N9,    KC.N0,   KC.MINUS,  KC.EQUAL, xxxxxxx,  KC.BSPC, KC.HOME,  xxxxxxx,
+        KC.TAB,   xxxxxxx, KC.Q,  KC.W,    KC.E,  KC.R,  KC.T,     KC.Y,  KC.U,  KC.I,     KC.O,    KC.P,      KC.LBRC,  KC.RBRC,  KC.BSLS, KC.PGUP,  xxxxxxx,
+        KC.CAPS,  xxxxxxx, KC.A,  KC.S,    KC.D,  KC.F,  KC.G,     KC.H,  KC.J,  KC.K,     KC.L,    KC.SCOLON, KC.QUOTE, KC.ENTER, KC.NO,   KC.PGDN,  xxxxxxx,
+        xxxxxxx,  KC.LSFT, KC.Z,  KC.X,    KC.C,  KC.V,  KC.B,     KC.N,  KC.M,  KC.COMMA, KC.DOT,  KC.SLASH,  xxxxxxx,  KC.RSFT,  KC.UP,   KC.END,   xxxxxxx,
+        KC.LCTRL, KC.LGUI, KC.NO, KC.LALT, xxxx,  xxxx,  KC.SPACE, xxxx,  xxxx,  xxxxxxx,  KC.RALT, Fn,        KC.RCTRL, KC.LEFT,  KC.DOWN, KC.RIGHT, xxxxxxx,
     ],
 
     # Fn (Layer 1)
     [
-        KC.NO,   KC.MUTE, KC.VOLD, KC.VOLU, KC.MPRV, KC.MPLY, KC.MNXT, KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.BRID, KC.BRIU, KC.PSCREEN, KC.PAUSE, KC.DELETE, KC.TRNS,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,    KC.TRNS,  KC.TRNS,   KC.TRNS,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,    KC.TRNS,  KC.TRNS,   KC.TRNS,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,    KC.TRNS,  KC.TRNS,   KC.TRNS,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,    KC.TRNS,  KC.TRNS,   KC.TRNS,
+        xxxxxxx, KC.MUTE, KC.VOLD, KC.VOLU, KC.MPRV, KC.MPLY, KC.MNXT, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC.BRID, KC.BRIU, KC.PSCR, KC.PAUS, KC.DEL,  _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     ]
 ]
 
 # volume knob
-encoder = EncoderHandler()
-encoder.pins = ((board.GP27, board.GP26),)
-encoder.map = [((KC.VOLU, KC.VOLD))]  # cw = vol up, ccw = vol down
-keyboard.modules.append(encoder)
+encoder_handler.pins = ((board.GP27, board.GP26),)
+encoder_handler.map = [
+    ((KC.VOLD, KC.VOLU),), # Volume
+    ((KC.BRID, KC.BRIU),), # Brightness (Fn)
+    ]
 
 if __name__ == '__main__':
     keyboard.go()
