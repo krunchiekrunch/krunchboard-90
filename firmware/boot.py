b19e04b9b384c05b1fa897c2c1e0f1c88628a770
krunch <75719839+krunchiekrunch@users.noreply.github.com>
2025-12-09 15:50:23 +0000

    firmware update

--- firmware/boot.py
+++ firmware/boot.py
@@ -1,4 +1,6 @@
 from kmk.bootcfg import bootcfg
+import usb_hid
+usb_hid.enable((usb_hid.Device.KEYBOARD,))
 
 bootcfg(
     usb_id={'manufacturer': "krunchiekrunch", 'product': "krunchboard-90"},
--- firmware/main.py
+++ firmware/main.py
@@ -1,26 +1,31 @@
 import board
-import digitalio
+# import digitalio
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import MatrixScanner
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
-from kmk.extensions.display import Display, TextEntry
-from kmk.extensions.display.ssd1306 import SSD1306
 from kmk.extensions.RGB import RGB, AnimationModes
 from kmk.modules.macros import Macros, Press, Release, Tap
 from kmk.modules.mouse_keys import MouseKeys
 from kmk.extensions.media_keys import MediaKeys
 from kmk.modules.encoder import EncoderHandler
 from kmk.modules.layers import Layers
+from kmk.modules.tapdance import TapDance
 
 keyboard = KMKKeyboard()
 macros = Macros()
 keyboard.modules.append(macros)
 mouse = MouseKeys()
+encoder_handler = EncoderHandler()
+tapdance = TapDance()
+tapdance.tap_time = 250
 keyboard.modules.append(MouseKeys())
 keyboard.extensions.append(MediaKeys())
-encoder_handler = EncoderHandler()
 keyboard.modules.append(encoder_handler)
+keyboard.modules.append(Layers())
+keyboard.modules.append(tapdance)
+
+keyboard.go_slow_when_no_keys_pressed = False
 
 # matrix
 keyboard.row_pins = (
@@ -59,21 +64,23 @@ xxxxxxx = KC.NO
 xxxx = KC.NO
 Fn = KC.MO(1)
 
+RMB = KC.TD(KC.MACRO(Release(KC.MB_RMB)), KC.MACRO(Press(KC.MB_RMB)),)
+RSW = KC.TD(Fn, KC.MUTE)
 
 keyboard.keymap = [
     # Base (Layer 0)
     [
-        KC.ESC,   KC.F1,   KC.F2, KC.F3,   KC.F4, KC.F5, KC.F6,    KC.F7, KC.F8, KC.F9,    KC.F10,  KC.F11,    KC.F12,   KC.NLCK,  KC.SLCK, KC.INS,   KC.MUTE,
-        KC.GRAVE, KC.N1,   KC.N2, KC.N3,   KC.N4, KC.N5, KC.N6,    KC.N7, KC.N8, KC.N9,    KC.N0,   KC.MINUS,  KC.EQUAL, xxxxxxx,  KC.BSPC, KC.HOME,  xxxxxxx,
-        KC.TAB,   xxxxxxx, KC.Q,  KC.W,    KC.E,  KC.R,  KC.T,     KC.Y,  KC.U,  KC.I,     KC.O,    KC.P,      KC.LBRC,  KC.RBRC,  KC.BSLS, KC.PGUP,  xxxxxxx,
-        KC.CAPS,  xxxxxxx, KC.A,  KC.S,    KC.D,  KC.F,  KC.G,     KC.H,  KC.J,  KC.K,     KC.L,    KC.SCOLON, KC.QUOTE, KC.ENTER, KC.NO,   KC.PGDN,  xxxxxxx,
-        xxxxxxx,  KC.LSFT, KC.Z,  KC.X,    KC.C,  KC.V,  KC.B,     KC.N,  KC.M,  KC.COMMA, KC.DOT,  KC.SLASH,  xxxxxxx,  KC.RSFT,  KC.UP,   KC.END,   xxxxxxx,
-        KC.LCTRL, KC.LGUI, KC.NO, KC.LALT, xxxx,  xxxx,  KC.SPACE, xxxx,  xxxx,  xxxxxxx,  KC.RALT, Fn,        KC.RCTRL, KC.LEFT,  KC.DOWN, KC.RIGHT, xxxxxxx,
+        KC.ESC,   KC.F1,   KC.F2, KC.F3,   KC.F4, KC.F5, KC.F6,    KC.F7, KC.F8, KC.F9,    KC.F10,  KC.F11,    KC.F12,   KC.PSCR,  KC.PAUS, KC.DEL,   RSW,
+        KC.GRAVE, KC.N1,   KC.N2, KC.N3,   KC.N4, KC.N5, KC.N6,    KC.N7, KC.N8, KC.N9,    KC.N0,   KC.MINUS,  KC.EQUAL, xxxxxxx,  KC.BSPC, KC.HOME,  KC.MUTE,
+        KC.TAB,   xxxxxxx, KC.Q,  KC.W,    KC.E,  KC.R,  KC.T,     KC.Y,  KC.U,  KC.I,     KC.O,    KC.P,      KC.LBRC,  KC.RBRC,  KC.BSLS, KC.PGUP,  KC.NO,
+        KC.CAPS,  xxxxxxx, KC.A,  KC.S,    KC.D,  KC.F,  KC.G,     KC.H,  KC.J,  KC.K,     KC.L,    KC.SCOLON, KC.QUOTE, KC.ENTER, KC.NO,   KC.PGDN,  KC.NO,
+        xxxxxxx,  KC.LSFT, KC.Z,  KC.X,    KC.C,  KC.V,  KC.B,     KC.N,  KC.M,  KC.COMMA, KC.DOT,  KC.SLASH,  xxxxxxx,  KC.RSFT,  KC.UP,   KC.END,   KC.NO,
+        KC.LCTRL, KC.LGUI, xxxx,  KC.LALT, xxxx,  xxxx,  KC.SPACE, xxxx,  xxxx,  xxxxxxx,  KC.RALT, Fn,        KC.RCTRL, KC.LEFT,  KC.DOWN, KC.RIGHT, RMB,
     ],
 
     # Fn (Layer 1)
     [
-        xxxxxxx, KC.MUTE, KC.VOLD, KC.VOLU, KC.MPRV, KC.MPLY, KC.MNXT, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC.BRID, KC.BRIU, KC.PSCR, KC.PAUS, KC.DEL,  _______,
+        xxxxxxx, KC.MPRV, KC.MPLY, KC.MNXT, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC.BRID, KC.BRIU, KC.NLCK, KC.SLCK, KC.INS,  _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -82,16 +89,16 @@ keyboard.keymap = [
 ]
 
 # volume knob
-encoder_handler.pins = ((board.GP27, board.GP26),)
+encoder_handler.pins = ((board.GP26, board.GP27),)
 encoder_handler.map = [
     ((KC.VOLD, KC.VOLU),), # Volume
     ((KC.BRID, KC.BRIU),), # Brightness (Fn)
     ]
 
 # pico gp25 led
-led = digitalio.DigitalInOut(board.GP25)
-led.direction = digitalio.Direction.OUTPUT
-led.value = True
+# led = digitalio.DigitalInOut(board.GP25)
+# led.direction = digitalio.Direction.OUTPUT
+# led.value = True
 
 if __name__ == '__main__':
     keyboard.go()
